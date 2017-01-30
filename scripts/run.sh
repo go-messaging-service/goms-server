b229e0e37d2e05239207133ce70a0b2bccdda010
hauke96 <mail@hauke-stieler.de>
2017-01-30 19:12:03 +0100

    run script: Added some comments on output.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-sh ./scripts/compile.sh
+echo "========="
+echo " COMPILE"
+echo "========="
+bash ./scripts/compile.sh
+echo ""
+echo "========="
+echo "  START"
+echo "========="
 ./bin/goMS-server
 
 # run "nc localhost 55545" or "sh connect.sh" to connect to the server via terminal
