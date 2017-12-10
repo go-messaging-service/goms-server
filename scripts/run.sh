b2f42c42d793c54743f390408cc6801b0a26482c
Hauke Stieler <mail@hauke-stieler.de>
2017-12-10 20:26:32 +0100

    Add error handling and cleanup to run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,31 @@
-#!/bin/sh
+#!/bin/bash
+
+echo "========="
+echo "  CLEAN"
+echo "========="
+echo "Removing old build ..."
+rm -r ./bin/*
+if [ $? -ne 0 ]; then
+	echo ""
+	echo "CLEANUP FAILED!"
+	exit 1
+fi
+echo "CLEANUP DONE"
+echo ""
 
 echo "========="
 echo " COMPILE"
 echo "========="
-bash ./scripts/compile.sh
+echo "Calling './scripts/compile.sh"
+sh ./scripts/compile.sh
+if [ $? -ne 0 ]; then
+	echo ""
+	echo "COMPILING FAILED!"
+	exit 2
+fi
+echo "COMPILING DONE"
 echo ""
+
 echo "========="
 echo "  START"
 echo "========="
