6d672f289f514a86d0560d6a984b9aad2fe6681e
Hauke Stieler <mail@hauke-stieler.de>
2018-08-30 01:52:43 +0200

    Write server log into RES directory

--- scripts/test/setup.sh
+++ scripts/test/setup.sh
@@ -17,7 +17,7 @@ rm -rf "$DIR_RES"
 mkdir "$DIR_RES"
 
 echo ">>> LAUNCH SERVER"
-nohup "$DIR_ROOT/run.sh" -c "$DIR_CASE/conf/server.json" 2>1 > "$DIR_CASE/server.log" &
+nohup "$DIR_ROOT/run.sh" -c "$DIR_CASE/conf/server.json" 2>1 > "$DIR_RES/server.log" &
 
 # Wait a bit for the server to come up
 for WAIT in $SRV_WAIT
